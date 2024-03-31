e8972e56d9486c9c44d370623f728fabd3a7d9b4
DJPajares <dj.pajares@gmail.com>
2024-03-31 12:36:29 +0800

    fix: missing routes and connections on index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import router from './routes';
 
 dotenv.config();
 
@@ -10,9 +12,12 @@ const databaseUrl = process.env.DATABASE_URL || 'mongodb://127.0.0.1/database';
 
 // Middleware
 app.use(express.json());
+app.use(router);
 
 const start = async () => {
   try {
+    await mongoose.connect(databaseUrl);
+
     app.listen(port, () => {
       console.log(`[server]: Server is running at http://localhost:${port}`);
     });
