599350a07cceebe75d4b351d2fb9e2c93ccfa0b6
Darwin Jason Pajares <xsgdp001@singlife.com>
2024-04-03 11:15:54 +0800

    feat: add constants for error handling

---
+++ src/utilities/constants.ts
@@ -0,0 +1,16 @@
+const constants = {
+  validations: {
+    category: {
+      name: {
+        required: 'Category name is required'
+      }
+    },
+    type: {
+      name: {
+        required: 'Type name is required'
+      }
+    }
+  }
+};
+
+export default constants;
